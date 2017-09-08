f88103c137b7b3b024eb08bf31ef79cbcc218150
Robyn Allen <enrobyn@users.noreply.github.com>
2017-09-08 09:56:24 -0400

    initial commit

---
+++ read_essays.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+'''
+    read_essays.py
+
+    This Python file can be used as a starting point for  
+    teaching the computer to grade a school essay.
+'''
+
+# Don't worry about these 2 lines
+from essays.en1 import essay_en1    
+from essays.en2 import essay_en2
+
+
+if "really cool" in essay_en1:
+    print("The phrase 'really cool' appears in Essay 1.")
+else:
+    print("The phrase 'really cool' was NOT found.")
