5ba08de3bfadcf0cb29e76c42d64800ae2cf66dc
Robyn Allen <enrobyn@users.noreply.github.com>
2017-09-09 09:20:06 -0400

    add Spanish versions of essays, add handouts

---
+++ essays/es1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+'''  es1.py                                                                     
+                                                                                
+     This Python file creates a variable called essay_es1                       
+     and stores a string into it.                                               
+                                                                                
+     The string is the text of a short essay in Spanish.                        
+                                                                                
+     You do not need to modify this file.                                       
+'''
+
+essay_es1 = """Pienso que los astronautas son geniales 
+
+
+¡GUAUU! ¡¿Estás listo para ir al espacio?! La Estación Espacial Internacional es un lugar que queda MUY, MUY lejos del planeta Tierra y donde la gente puede estar en el espacio exterior. Es realmente genial y ostentoso. En realidad, no es algo muy nuevo, aunque no estoy seguro de cuando se hizo. Probablemente se hizo miles de meses antes de que yo naciera.
+
+Esta semana es importante porque una astronauta que estuvo en la Estación Espacial Internacional durante mucho, mucho tiempo vuelve a la Tierra. Se llama Peggy Whitson. Peggy es ingeniera y tiene mucha experiencia en vuelos espaciales. ¡En este viaje a la estación espacial rompió el récord mundial porque estuvo en el espacio durante más de 9 meses! ¡¡¡¿Puedes imaginarlo?!!! Yo, sí. Vivir en el espacio durante tanto tiempo es TERRIBLEMENTE difícil . Ella es una campeona del espacio exterior. Me gustan los campeones. Mi profesor de educación física de tercer grado dice que todos somos campeones.
+
+La Estación Espacial Internacional es muy veloz. Va a 17 500 millas por hora, lo que es más rápido que casi cualquier cosa que manejemos en la Tierra. ¡Excepto en mis sueños! En mis sueños vuelo más rápido que mil balas rápidas. Pero, ¿por qué "balas"? Estamos hablando del espacio, y en el espacio no hay balas, ¿cierto? Cierto. Mi tía Sumayyah conduce un convertible porque abrió una compañía de software importante y le pagan mucho dinero. ¡Ni siquiera el convertible de Sumayyah puede ir a 17 500 millas por hora! Pero me imagino que sus sueños sí. DE VERDAD.
+
+En esta misión en la estación espacial, Peggy estuvo acompañada por otros dos astronautas. Los astronautas de la estación espacial son como una familia porque viven juntos, trabajan juntos y comen juntos. Me gusta la familia. Y los cerdos. Y el espacio. ¡Guau!, ¿y a ti?
+
+Los astronautas tienen mucha suerte de poder vivir en un lugar tan genial. Así que esta semana es muy importante porque se trata del espacio y de la velocidad, y de la familia y del amor, y el amor es fabuloso.
